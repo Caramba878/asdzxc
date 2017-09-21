3a60ccfe1e581e22c2795fd3a6abcec69fc99376
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2017-09-21 14:15:27 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,6 +1,6 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412");
+    $conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412dan");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE registration_tbl(
     id INT NOT NULL IDENTITY(1,1) 
