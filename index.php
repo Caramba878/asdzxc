e4cb6e57fc12f981ed040169df67a5c230ebb14b
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2017-11-21 10:15:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,35 @@ name="submit" value="Submit" />
 </form>
 <?php
 
+try {
+    $conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412danNN");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3>Your're registered!</h3>";
+}
 
 $sql_select = "SELECT * FROM registration_tbl";
 $stmt = $conn->query($sql_select);
