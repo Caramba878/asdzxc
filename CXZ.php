53a2abc0e53aa6d3b1efc64c1517b09333149205
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2017-11-24 14:57:53 +0400

    Update CXZ.php

--- CXZ.php
+++ CXZ.php
@@ -3,11 +3,18 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412danNN");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE city_table(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    City VARCHAR(10)
-    $conn->query($sql);
-}
-}
-echo "123";
+  $arr[1] = "Первый";
+  $arr[2] = "Второй";
+  $arr[3] = "Третий";
+  $arr[4] = "Четвёртый";
+  if(count($arr) > 0)
+  {
+    echo "<select name=id_catalog onchange='show(this.form.id_catalog)'>";
+    echo "<option value=0>Не имеет значения</option>";
+    foreach($arr as $index => $value)
+    {
+      echo "<option value=$index>$value</option>";
+    }
+    echo "</select>";
+ }
 ?>
