f35beafbf2122099cbb6a407890755664c264f80
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2017-12-19 11:04:41 +0400

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ try {
     
     // Insert data
     $sql_insert = 
-"INSERT INTO registration_tbl (name, email, date) 
+"INSERT INTO registration_tb (name, email, date) 
                    VALUES (?,?,?)";
     $stmt = $conn->prepare($sql_insert);
     $stmt->bindValue(1, $name);
@@ -74,7 +74,7 @@ catch(Exception $e) {
 echo "<h3>Your're registered!</h3>";
 }
 
-$sql_select = "SELECT * FROM registration_tbl";
+$sql_select = "SELECT * FROM registration_tb";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
 if(count($registrants) > 0) {
