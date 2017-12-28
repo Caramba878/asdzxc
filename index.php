b9450d3ae9e65fb25d7eb908d6977d2c25b5c298
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2017-12-28 12:56:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -103,26 +103,24 @@ $city = $_POST['city'];
 $sql_select = "SELECT * FROM registration_on WHERE city like :city";
 $stmt = $conn->prepare($sql_select);
 $stmt->execute(array(':city'=>$city.'%'));
-
 }
-$registrants = $stmt->fetchAll(); 
-
+$registrants = $stmt->fetchAll();
 if(count($registrants) > 0) {
-    echo "<h2>People who are registered:</h2>";
-    echo "<table>";
-    echo "<tr><th>Name</th>";
-    echo "<th>Email</th>";
-echo "<th>City</th>";
-    echo "<th>Date</th></tr>";
-  
-    foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-        echo "<td>".$registrant['email']."</td>";
-        echo "<td>".$registrant['city']."</td>";
-         echo "<td>".$registrant['date']."</td></tr>";
-    }
-    echo "</table>";
-} else {
+echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<table>";
+echo "<tr><th>Name</th>";
+echo "<th>Email</th>";
+echo "<th>city</th>";
+echo "<th>Date</th></tr>";
+foreach($registrants as $registrant) {
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['city']."</td>";
+echo "<td>".$registrant['date']."</td></tr>";
+}
+echo "</table>";
+}
+else {
     echo "<h3>No one is currently registered.</h3>";
 }
 
